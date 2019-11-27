6501a94a724a3cb3f228894cc47996e54f72c3c3
Moritz Wicki <wickimoritz09@gmail.com>
2019-11-27 17:00:45 +0100

    Push nr 2 | kann Beiträge anzeigen

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ $formSet = false;
 $user = 'root';
 $password = 'root';
 
+
 $pdo = new PDO('mysql:host=localhost;dbname=blog', $user, $password, [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
